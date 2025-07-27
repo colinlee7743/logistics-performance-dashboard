a09495d0534dad44f6ea731ef9df2fa46fc1e8b2
Colin Lee <colinlee7743@gmail.com>
2025-07-27 14:52:06 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,6 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.title(🚛 Logistics Performance Dashboard')
+st.title('🚛 Logistics Performance Dashboard')
 
 st.markdwon('Real-time insights into delivery operations and performance')
