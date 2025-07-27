4a7750588682f09f2476fc3929569d5234c727ff
Colin Lee <colinlee7743@gmail.com>
2025-07-27 15:59:15 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,3 +39,18 @@ df = load_data()
 # Set up input widgets
 st.logo(image="images/streamlit-logo-primary-colormark-lighttext.png", 
         icon_image="images/streamlit-mark-color.png")
+
+with st.sidebar:
+    st.title("YouTube Channel Dashboard")
+    st.header("⚙️ Settings")
+    
+    max_date = df['DATE'].max().date()
+    default_start_date = max_date - timedelta(days=365)  # Show a year by default
+    default_end_date = max_date
+    start_date = st.date_input("Start date", default_start_date, min_value=df['DATE'].min().date(), max_value=max_date)
+    end_date = st.date_input("End date", default_end_date, min_value=df['DATE'].min().date(), max_value=max_date)
+    time_frame = st.selectbox("Select time frame",
+                              ("Daily", "Weekly", "Monthly", "Quarterly"),
+    )
+    chart_selection = st.selectbox("Select a chart type",
+                                   ("Bar", "Area"))
